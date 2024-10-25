e6292c8f9bd8f89c0b6c3b292428502c52b6abd3
miral <miral.yousef@mail.utoronto.ca>
2024-10-25 15:26:34 -0400

    Editing the Login Use Case (Task 2)

--- src/main/java/app/MainWithInMemory.java
+++ src/main/java/app/MainWithInMemory.java
@@ -69,3 +69,4 @@ public class MainWithInMemory {
         application.setVisible(true);
     }
 }
+
--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user to name.
+     * @param name to point at user
+     */
+    void setCurrentUser(String name);
 }
