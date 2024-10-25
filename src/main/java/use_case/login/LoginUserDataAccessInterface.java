d014980c763eafb728008fb226cfaed6c38ba7f1
miral <miral.yousef@mail.utoronto.ca>
2024-10-25 15:50:47 -0400

    Updated Unit Tests (Task 2)

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -32,4 +32,10 @@ public interface LoginUserDataAccessInterface {
      * @param name to point at user
      */
     void setCurrentUser(String name);
+
+    /**
+     * Gets the name of the current user.
+     * @return the name of the current user or null if there are no users.
+     */
+    String getCurrentUser();
 }
